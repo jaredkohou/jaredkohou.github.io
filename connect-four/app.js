01908bd3599f724c577f1cdad60b8fb7c9471677
Jared <jaredkohou@gmail.com>
2021-02-10 09:48:08 -0600

    createGrid addded

--- connect-four/app.js
+++ connect-four/app.js
@@ -3,12 +3,17 @@ class Grid {
         this.rows = 6;
         this.cols = 7;
         this.selector = selector
-        
+        this.createGrid()
     }
 
     createGrid(){
         for (let rows = 1; row < 7; row++) {
             const $r = $("<div></div>").addClass("row")
+            for (let col = 1; col < 8; col++){
+                const $c = $("<div></div").addClass("col empty").attr("data-col", col).attr("data-row", row);
+                $r.append($c)
+            }
+            $(this.selector).append($r)
         }
     }
 
