8ce4753c7eaac02a77e1cd9273f9623a50e55dfa
Jared <jaredkohou@gmail.com>
2021-02-11 22:58:45 -0600

    Basic grid layeout

--- connect-four/app.js
+++ connect-four/app.js
@@ -7,41 +7,9 @@ class Grid {
         this.setupListners()
     }
 
-    createGrid() {
-        for (let row = 1; row < 7; row++) {
-            const $r = $("<div></div>").addClass('row')
-            for (let col = 1; col < 8; col++) {
-                const $c = $("<div></div>").addClass("col empty").attr("data-col", col).attr("data-row", row);
-                $r.append($c)
-            }
-            $(this.selector).append($r)
-        }
-    }
 
-
-
-    setupListners() {
-
-        function getEmptyCircle(col) {
-            const cellsArr = $(`.col[data-col='${col}']`);
-            for (let i = cellsArr.length - 1; i >= 0; i--) {
-                const $c = $(cellsArr[i])
-                if ($c.hasClass("empty")) {
-                    return $c
-                }
-            }
-            return null
-        }
-
-        $(".col.empty").hover(function() {
-            
-            const col = $(this).data("col")
-            let emptycell = getEmptyCircle(col)
-            emptycell.toggleClass("faded__red")
-        })
-    }
 }
-/* Adding the last parts now*/
+
 
 
 
