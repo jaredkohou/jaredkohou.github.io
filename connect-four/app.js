573771b53d527bb3e399c0b83a82b31ea5c854e9
Jared <jaredkohou@gmail.com>
2021-02-11 23:02:10 -0600

    creating grid dinamicaly

--- connect-four/app.js
+++ connect-four/app.js
@@ -6,6 +6,19 @@ class Grid {
         this.createGrid()
         this.setupListners()
     }
+    createGrid() {
+        $(this.selector).empty();
+        this.isGameOver = false;
+        this.turn = "red"
+        for (let row = 0; row < this.rows; row++) {
+            const $r = $("<div>").addClass('row')
+            for (let col = 0; col < this.cols; col++) {
+                const $c = $("<div>").addClass("col empty").attr("data-col", col).attr("data-row", row);
+                $r.append($c)
+            }
+            $(this.selector).append($r)
+        }
+    }
 
 
 }
